1bad5ede7837d104db14c948dfab903236a4da9a
ArgeMora <argemoraj@hotmail.com>
2025-02-06 22:15:31 -0600

    Primeras lineas de código

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombres = []
+
+function agregarNombres() {
+    let nombresAmigos = document.getElementById("amigo");
+
+    
+}
